ef7c15041d4cf69cc573741c25e5a256fe87369e
FamilyFriendly77 <116194178+FamilyFriendly77@users.noreply.github.com>
2025-07-06 02:46:47 +0200

    singin page styling

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -9,6 +9,19 @@ import { useState } from 'react';
 const Icons = new Map();
 
 Icons.set('GitHub', <GithubIcon />);
+Icons.set(
+  'Google',
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    x='0px'
+    y='0px'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+  >
+    <path d='M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032 s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2 C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z'></path>
+  </svg>
+);
 
 export default function SignIn({
   providers,
@@ -32,19 +45,26 @@ export default function SignIn({
     });
   };
   return (
-    <div className='w-screen h-screen bg-linear-to-tr from-[#00A1E0] to-[#0CAC64] bg-fixed'>
-      <div className='w-128 h-160 bg-[#EBEBEB] rounded-4xl flex flex-col items-center p-6'>
-        <h1 className='font-bold text-3xl'>Sign In</h1>
+    <div className='w-screen h-screen bg-linear-to-tr from-[#00A1E0] to-[#0CAC64] bg-fixed flex items-center justify-center text-[#2A2F3C]'>
+      <div className='w-128 h-128 bg-[#EBEBEB] rounded-4xl flex flex-col items-center p-6 shadow-2xl'>
+        <h1 className='font-bold text-4xl mb-12 mt-4'>
+          {isSignIn ? 'Sign In' : 'Sign Up'}
+        </h1>
         {isSignIn ? (
-          <div className='flex flex-col w-full h-fit'>
-            <form onSubmit={(e) => e.preventDefault()}>
+          <div className='flex flex-col w-full h-fit mb-6 justify-center items-center'>
+            <form
+              onSubmit={(e) => e.preventDefault()}
+              className='flex flex-col justify-center items-center gap-2 mb-2'
+            >
               <input
+                className='border-2 p-2 rounded-2xl font-semibold'
                 type='text'
                 placeholder='Email'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
               <input
+                className='border-2 p-2 rounded-2xl font-semibold'
                 type='password'
                 placeholder='Password'
                 value={password}
@@ -53,6 +73,8 @@ export default function SignIn({
               <input
                 type='submit'
                 onClick={SubmitSignIn}
+                className='py-2 px-4 text-[#EBEBEB] bg-[#FF1F1F] rounded-3xl font-semibold'
+                value={'Sign In'}
               />
             </form>
             <span>
@@ -62,27 +84,33 @@ export default function SignIn({
                   setIsSignIn(false);
                 }}
               >
-                Sign In
+                Sign Up
               </button>
             </span>
           </div>
         ) : (
-          <div>
+          <div className='flex flex-col w-full h-fit mb-6 justify-center items-center'>
             {' '}
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form
+              onSubmit={(e) => e.preventDefault()}
+              className='flex flex-col justify-center items-center gap-2 mb-2'
+            >
               <input
+                className='border-2 p-2 rounded-2xl font-semibold'
                 type='text'
                 placeholder='Email'
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />{' '}
               <input
+                className='border-2 p-2 rounded-2xl font-semibold'
                 type='text'
                 placeholder='name'
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
               <input
+                className='border-2 p-2 rounded-2xl font-semibold'
                 type='password'
                 placeholder='Password'
                 value={password}
@@ -91,21 +119,34 @@ export default function SignIn({
               <input
                 type='submit'
                 onClick={SubmitSignUp}
+                className='py-2 px-4 text-[#EBEBEB] bg-[#FF1F1F] rounded-3xl font-semibold'
+                value={'Sign Up'}
               />
             </form>
+            <span>
+              Already have an account?{' '}
+              <button
+                onClick={() => {
+                  setIsSignIn(true);
+                }}
+              >
+                Sign In
+              </button>
+            </span>
           </div>
         )}
-        <div>
+        <div className='w-[90%] h-[2px] rounded-2xl bg-[#171A21] mb-4'></div>
+        <div className='flex flex-col justify-center items-center gap-2'>
           {Object.values(providers).map((provider) => {
             if (provider.name != 'Credentials') {
               return (
                 <button
                   key={provider.name}
                   onClick={() => signIn(provider.id)}
-                  className='py-1 px-5 border-2 border-solid rounded-md flex flex-row gap-2'
+                  className='py-1 px-5 border-2 border-solid rounded-md flex flex-row gap-2 font-semibold'
                 >
                   {Icons.get(provider.name)}
-                  Sign In with {provider.name}
+                  Continue with {provider.name}
                 </button>
               );
             }
