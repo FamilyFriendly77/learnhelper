fa230fbbadc96e8e6782ba4db6d3b04f6dda86b6
FamilyFriendly77 <116194178+FamilyFriendly77@users.noreply.github.com>
2025-07-17 17:59:59 +0200

    chat component begining

--- src/pages/roadmap/[skill].tsx
+++ src/pages/roadmap/[skill].tsx
@@ -7,8 +7,11 @@ import {
   RoadmapType,
 } from '@/app/(ShowDataAndTypes)/RoadmapTypes';
 import Roadmap from '@/app/(components)/Roadmap';
+import { ArrowDown, ArrowUp } from 'lucide-react';
+import { useState } from 'react';
 
 export default function RoadmapPage({ roadmap }: { roadmap: RoadmapType }) {
+  const [chatOpen, setChatOpen] = useState(false);
   return (
     <Layout>
       <div className='w-full  grow-1 flex items-center justify-center'>
@@ -17,6 +20,29 @@ export default function RoadmapPage({ roadmap }: { roadmap: RoadmapType }) {
             <Roadmap Roadmap={roadmap as RoadmapType} />
           </div>
         </div>
+      </div>
+      <div className='fixed bottom-0 right-32 w-96 h-fit flex-col'>
+        <div
+          className=' w-96 h-16 flex justify-between px-8 items-center text-2xl font-bold text-[#EBEBEB] bg-[#FF1F1F] rounded-t-3xl'
+          onClick={() => {
+            setChatOpen(!chatOpen);
+          }}
+        >
+          <span>Ask a question</span>
+          {chatOpen ? (
+            <ArrowDown
+              color='#EBEBEB'
+              size={32}
+            />
+          ) : (
+            <ArrowUp
+              className=''
+              color='#EBEBEB'
+              size={32}
+            />
+          )}
+        </div>
+        {chatOpen ? <div className='w-full h-96 border-1'></div> : null}
       </div>
     </Layout>
   );
